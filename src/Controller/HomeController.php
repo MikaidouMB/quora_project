a5ebaf3c9accac8708e908ebbe3529351abee8fd
Mikaïdou <mikaidoumbo@hotmail.fr>
2022-11-02 15:10:48 +0100

    add home controller render index

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,12 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/home', name: 'home')]
     public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/HomeController.php',
-        ]);
+       return $this->render('home/index.html.twig',[
+           'controller_name' => 'HomeController',
+       ]);
     }
 }
